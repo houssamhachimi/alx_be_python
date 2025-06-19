6a0275d952a3c414539987e42430f20b0c725e57
Houssam Hachimi <houssamhachimi84@gmail.com>
2025-06-19 22:59:34 +0100

    .

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,7 +8,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(1, 2), 3)
         self.assertEqual(self.calc.add(-1, -2), -3)
         self.assertEqual(self.calc.add(-1, 2), 1)
